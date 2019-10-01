f78e8bba4b7ccd524b05621e04b8c13bef6ea003
DanielRSnell <drsnell711@gmail.com>
2019-10-01 15:06:46 -0500

    update

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Update WP Post Type Fields
+
+## Instructions:
+
+### Git Clone Repo
+### Once inside the repo run node update.js PageNumber
+Page number can be replace with which page you would like to run it on, this is to ensure no mass deletion. 
+
+Example: node update.js 2
+
+This updates all communities on page 2 within WordPress.
--- update.js
+++ update.js
@@ -1,46 +1,150 @@
 const WPAPI = require("wpapi");
 const axios = require("axios");
+const args = require("minimist")(process.argv.slice(2));
+require("dotenv").config();
 
 var wp = new WPAPI({
-	endpoint: "http://192.249.121.250/~seldev/wp-json",
-	username: `${process.env.USERNAME}`,
-	password: `${process.env.PASSWORD}`
+	endpoint: "http://199.250.204.45/~seldev/wp-json",
+	username: `selectgroup`,
+	password: `Its23452!@#`
 });
 
-async function UpdateProperties() {
-	await wp.posts().categories(6).perPage(100).page(1).then((data) => {
-		data.forEach((item, index) => {
-			let update = {
-				arch_url: item.acf["ach_automatic_debit_from_checking"].url || null,
-				architectural_url: item.acf["architectural_change_application"].url || null,
-				complaint_procedure_url: item.acf["complaint_procedure_and_process"].url || null,
-				complaint_url: item.acf["complaint_form"].url || null,
-				owner_url: item.acf["owner_information"].url || null,
-				tenant_url: item.acf["tenant_information_form"].url || null,
-				vehicle_url: item.acf["vehicle_information_form"].url || null,
-				pet_url: item.acf["pet_information_form"].url || null,
-				asc_url: item.acf["asc_application_form"].url || null,
-				office_number: item.acf["office_number"].url || null,
-				app_url: item.acf["application_form"].url || null,
-				policy_url: item.acf["policy_cost_form"].url || null,
-				pool_url: item.acf["pool_rules"].url || null,
-				policy_record_url: item["policy_record"].url || null,
-				rules_url: item["rules_&_regulations"].url || null,
-				parking_url: item.acf["parking_rules"].url || null,
-				homeowner_url: item.acf["homeowner_permit_guide_form"].url || null,
-				policy_reso_url: item.acf["policy_resolution"].url || null,
-				boat_url: item.acf["boat_slip_form"].url || null
-			};
+// "ach_automatic_debit_from_checking"
+// "architectural_change_application"
+//  "complaint_procedure_and_process"
+// "complaint_form"
+// "owner_information"
+// "tenant_information_form"
+// "vehicle_information_form"
+// "pet_information_form"
+// "asc_application_form"
+// "application_form"
+// "policy_cost_form"
+//  "pool_rules"
+// "policy_record",
+// "rules_&_regulations",
+//  "parking_rules"
+//  "homeowner_permit_guide_form"
+//  "policy_resolution"
+// "boat_slip_form"
 
-			console.log(update);
-			// wp
-			// 	.posts()
-			// 	.id(item.id)
-			// 	.update(item)
-			// 	.then((x) => console.log(`${item.id} updated`))
-			// 	.catch((e) => console.log(e));
-		});
+const apiPromise = WPAPI.discover(`${process.env.ENDPOINT}`).then(function(site) {
+	return site.auth({
+		username: `${process.env.USER}`,
+		password: `${process.env.PASS}`
 	});
+});
+
+async function UpdateWordpress(site, id, arr) {
+	await site
+		.community()
+		.id(id)
+		.update({ fields: { managers_email: "", community_forms: arr } })
+		.then((response) => console.log(response.id))
+		.catch((e) => console.log("Error"));
 }
 
-UpdateProperties();
+apiPromise.then(function(site) {
+	console.log("start");
+	site.community().perPage(20).page(parseInt(args["_"])).then((data) => {
+		data.forEach((item, index) => {
+			const arr = [];
+
+			if (item.acf["ach_automatic_debit_from_checking"] !== false) {
+				arr.push({
+					form_name: "ACH Automatic Debit",
+					form_file: item.acf["ach_automatic_debit_from_checking"]
+				});
+			}
+			if (item.acf["architectural_change_application"] !== false) {
+				arr.push({
+					form_name: "Architectural Change Application",
+					form_file: item.acf["architectural_change_application"]
+				});
+			}
+			if (item.acf["complaint_procedure_and_process"] !== false) {
+				arr.push({
+					form_name: "Complaint Procedure and Process",
+					form_file: item.acf["complaint_procedure_and_process"]
+				});
+			}
+			if (item.acf["complaint_form"] !== false) {
+				arr.push({
+					form_name: "Comaplint Form",
+					form_file: item.acf["complaint_form"]
+				});
+			}
+			if (item.acf["owner_information"] !== false) {
+				arr.push({
+					form_name: "Owner Information",
+					form_file: item.acf["owner_information"]
+				});
+			}
+			if (item.acf["tenant_information_form"] !== false) {
+				arr.push({
+					form_name: "Tenant Information Form",
+					form_file: item.acf["tenant_information_form"]
+				});
+			}
+			if (item.acf["vehicle_information_form"] !== false) {
+				arr.push({
+					form_name: "Vehicle Information Form",
+					form_file: item.acf["vehicle_information_form"]
+				});
+			}
+			if (item.acf["pet_information_form"] !== false) {
+				arr.push({
+					form_name: "Pet Information Form",
+					form_file: item.acf["pet_information_form"]
+				});
+			}
+			if (item.acf["asc_application_form"] !== false) {
+				arr.push({
+					form_name: "ASC Application Form",
+					form_file: item.acf["asc_application_form"]
+				});
+			}
+			if (item.acf["application_form"] !== false) {
+				arr.push({
+					form_name: "Application Form",
+					form_file: item.acf["application_form"]
+				});
+			}
+			if (item.acf["policy_cost_form"] !== false) {
+				arr.push({
+					form_name: "Policy Cost Form",
+					form_file: item.acf["policy_cost_form"]
+				});
+			}
+
+			if (item.acf["policy_record"] !== false) {
+				arr.push({
+					form_name: "Policy Record",
+					form_file: item.acf["policy_record"]
+				});
+			}
+
+			if (item.acf["homeowner_permit_guide_form"] !== false) {
+				arr.push({
+					form_name: "Homeowner Permit Guide Form",
+					form_file: item.acf["homeowner_permit_guide_form"]
+				});
+			}
+			if (item.acf["policy_resolution"] !== false) {
+				arr.push({
+					form_name: "Policy Resolution",
+					form_file: item.acf["policy_resolution"]
+				});
+			}
+			if (item.acf["boat_slip_form"] !== false) {
+				arr.push({
+					form_name: "Boat Slip Form",
+					form_file: item.acf["boat_slip_form"]
+				});
+			}
+
+			const reset = [];
+			UpdateWordpress(site, item.id, arr);
+		});
+	});
+});
